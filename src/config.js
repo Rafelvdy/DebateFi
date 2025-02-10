b24da678c8d0b5a8e75c588c3455874442b920c9
milesmitchell <milesmitchell345@gmail.com>
2025-02-10 21:23:35 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://debatefi-19.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://debatefi-19.onrender.com';
 export default API_BASE_URL;
